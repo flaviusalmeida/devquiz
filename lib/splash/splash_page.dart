1745bbe182c744eca5b1653c080a37c780195267
Flavius Almeida <flavius.filipe@gmail.com>
2021-04-19 23:02:06 -0300

    Aula 01

--- lib/splash/splash_page.dart
+++ lib/splash/splash_page.dart
@@ -1,8 +1,19 @@
+import 'package:DevQuiz/core/app_gradients.dart';
 import 'package:flutter/material.dart';
+import 'package:DevQuiz/core/app_images.dart';
 
 class SplashPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: AppGradients.linear,
+        ),
+        child: Center(
+          child: Image.asset(AppImages.logo),
+        ),
+      ),
+    );
   }
 }
